929ad52dba9bd42d35172f63dbcefcbf11de7bc2
Melissa Hookey <melissah315@gmail.com>
2022-04-28 19:21:49 -0500

    organized files

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const pageTemplate = require("./src/page-template");
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require("inquirer");
+
